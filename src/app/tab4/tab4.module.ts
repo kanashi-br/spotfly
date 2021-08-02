7cc986ce39f7921a94aae53e94886b13c43c93fb
Marlison S. S <marlison.silva@escolar.ifrn.edu.br>
2021-08-02 17:04:27 -0300

    tab bar menu criado e configurado

---
+++ src/app/tab4/tab4.module.ts
@@ -0,0 +1,23 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+import { Tab4PageRoutingModule } from './tab4-routing.module';
+
+import { Tab4Page } from './tab4.page';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    ExploreContainerComponentModule,
+    RouterModule.forChild([{ path: '', component: Tab4Page }]),
+    Tab4PageRoutingModule,
+  ],
+  declarations: [Tab4Page]
+})
+export class Tab4PageModule {}
